5302f252c0a99a1e7466a90db6ea570f9192dc71
Oumaima Draouil <draouil.oumaima@gmail.com>
2022-09-23 13:05:56 +0200

    first

---
+++ react1/src/components/Menu.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+const Menu = () => {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="white" >
+                <Container>
+                    {/* <Navbar.Brand href="#home">HOME</Navbar.Brand> */}
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#features">LOGIN PAGE</Nav.Link>
+                            {/* <Nav.Link href="#pricing">ReactProject</Nav.Link> */}
+                            {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action1</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Action2</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Action3</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Action4</NavDropdown.Item>
+                            </NavDropdown> */}
+                        </Nav>
+                        <Nav>
+                            <Nav.Link eventKey={2} href="#memes">
+                                GoMyCode CheckPoint
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    )
+}
+
+export default Menu
